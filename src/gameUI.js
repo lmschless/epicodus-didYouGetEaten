9c82c9fe488f23c046ad9e6d3dbd70332ef387b4
Luke <lmschless@gmail.com>
2020-03-10 16:54:50 -0700

    reformated code

--- src/gameUI.js
+++ src/gameUI.js
@@ -1,19 +1,22 @@
 import $ from 'jquery';
 import { HungryBear } from './hungrybear.js';
 $(document).ready(function() {
-let newBear;
+	let newBear;
 
 	$('#game-setup').submit(function(e) {
 		// const currentHunger = $('#current-hunger').val();
-		newBear = new HungryBear($('#name').val(), $("#difficulty-level").val().toLowerCase(), $('#hardcore-mode').is(':checked'));
+		newBear = new HungryBear(
+			$('#name').val(),
+			$('#difficulty-level').val().toLowerCase(),
+			$('#hardcore-mode').is(':checked')
+		);
 		newBear.playGame();
 		console.log(newBear);
 		e.preventDefault();
 	});
 
-	$('#feed').click(function (e) { 
+	$('#feed').click(function(e) {
 		e.preventDefault();
 		newBear.feed();
 	});
 });
-
--- src/hungrybear.js
+++ src/hungrybear.js
@@ -4,7 +4,7 @@ import './gameUI.js';
 import $ from 'jquery';
 
 export class HungryBear {
-	constructor(name, difficultyLevel, hardcore='no') {
+	constructor(name, difficultyLevel, hardcore = 'no') {
 		this.name = name;
 		this.foodLevel = 100;
 		this.sleepLevel = 100;
@@ -13,63 +13,62 @@ export class HungryBear {
 	}
 
 	setHunger() {
-		if(this.hardcore){
-				let hInterval =  setInterval(() => {
-				if(this.foodLevel == 0){
+		if (this.hardcore) {
+			let hInterval = setInterval(() => {
+				if (this.foodLevel == 0) {
 					clearInterval(hInterval);
 				}
 				$('#current-hunger').val(this.foodLevel);
 				this.currentHunger = this.foodLevel;
 				this.foodLevel--;
-			}, 50); 
-		}
-		else if(this.difficultyLevel == 'easy'){
-				let eInterval = setInterval(() => {
-				if(this.foodLevel == 0){
+			}, 50);
+		} else if (this.difficultyLevel == 'easy') {
+			let eInterval = setInterval(() => {
+				if (this.foodLevel == 0) {
 					clearInterval(eInterval);
 				}
-			$('#current-hunger').val(this.foodLevel);
-			this.currentHunger = this.foodLevel;
-			this.foodLevel--;
-		}, 1000);
-	} else if(this.difficultyLevel == 'normal'){
+				$('#current-hunger').val(this.foodLevel);
+				this.currentHunger = this.foodLevel;
+				this.foodLevel--;
+			}, 1000);
+		} else if (this.difficultyLevel == 'normal') {
 			let nInterval = setInterval(() => {
-			if(this.foodLevel == 0){
-				clearInterval(nInterval);
-			}
-			$('#current-hunger').val(this.foodLevel);
-			this.currentHunger = this.foodLevel;
-			this.foodLevel--;
-		}, 400);
-	} else {
-				let hardInterval = setInterval(() => {
-				if(this.foodLevel == 0){
+				if (this.foodLevel == 0) {
+					clearInterval(nInterval);
+				}
+				$('#current-hunger').val(this.foodLevel);
+				this.currentHunger = this.foodLevel;
+				this.foodLevel--;
+			}, 400);
+		} else {
+			let hardInterval = setInterval(() => {
+				if (this.foodLevel == 0) {
 					clearInterval(hardInterval);
 				}
-			$('#current-hunger').val(this.foodLevel);
-			this.currentHunger = this.foodLevel;
-			this.foodLevel--;
-		}, 100);
-	}
+				$('#current-hunger').val(this.foodLevel);
+				this.currentHunger = this.foodLevel;
+				this.foodLevel--;
+			}, 100);
+		}
 	}
 
 	feed() {
-		if(this.hardcore){
-			this.foodLevel +=4;
-		} else{
+		if (this.hardcore) {
+			this.foodLevel += 4;
+		} else {
 			this.foodLevel += 10;
 		}
 	}
 
 	setSleep() {
 		setInterval(() => {
-			while(this.sleepLevel >= 0){
+			while (this.sleepLevel >= 0) {
 				this.sleepLevel--;
 			}
 		}, 5000);
 	}
 
-	playGame(){
+	playGame() {
 		this.setHunger();
 	}
 }
