9ce4d33901fc29dff6dda897b0fb00aac748eded
Luke <lmschless@gmail.com>
2020-03-10 16:19:57 -0700

    finally got the counter to show in the current hunger input. Started work on difficulty options with different interval counters.

--- src/gameUI.js
+++ src/gameUI.js
@@ -1,13 +1,19 @@
 import $ from 'jquery';
 import { HungryBear } from './hungrybear.js';
 $(document).ready(function() {
+let newBear;
 
 	$('#game-setup').submit(function(e) {
-		const currentHunger = $('#current-hunger').val();
-		const newBear = new HungryBear($('#name').val(), 'normal');
+		// const currentHunger = $('#current-hunger').val();
+		newBear = new HungryBear($('#name').val(), 'normal');
 		newBear.playGame();
 		console.log(newBear);
 		e.preventDefault();
+	});
+
+	$('#feed').click(function (e) { 
+		e.preventDefault();
+		newBear.feed();
 	});
 });
 
--- src/hungrybear.js
+++ src/hungrybear.js
@@ -4,20 +4,37 @@ import './gameUI.js';
 import $ from 'jquery';
 
 export class HungryBear {
-	constructor(name, currentHunger) {
+	constructor(name, difficultyLevel) {
 		this.name = name;
 		this.foodLevel = 100;
 		this.sleepLevel = 100;
-		this.difficultyLevel;
-		this.currentHunger = currentHunger;
+		this.difficultyLevel = difficultyLevel;
 	}
 
 	setHunger() {
+		if(this.difficultyLevel == 'easy'){
+			setInterval(() => {
+			this.foodLevel--;
+			$('#current-hunger').val(this.foodLevel);
+			this.currentHunger = this.foodLevel;
+		}, 1000);
+	} else if(this.difficultyLevel == 'normal'){
 		setInterval(() => {
 			this.foodLevel--;
+			$('#current-hunger').val(this.foodLevel);
 			this.currentHunger = this.foodLevel;
-			console.log(this.foodLevel);
-		}, 1000);
+		}, 400);
+	} else {
+			setInterval(() => {
+			this.foodLevel--;
+			$('#current-hunger').val(this.foodLevel);
+			this.currentHunger = this.foodLevel;
+		}, 100);
+	}
+	}
+
+	feed() {
+		this.foodLevel += 10;
 	}
 
 	setSleep() {
